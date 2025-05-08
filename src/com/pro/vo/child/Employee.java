a5a990f2c39e0a7e3f3ac7b6ebde740f84c55ae3
sunhokim1 <92263333+sunhokim1@users.noreply.github.com>
2025-05-08 11:37:32 +0900

    add : Employee
    Employee vo 추가 (User에서 상속)

---
+++ src/com/pro/vo/child/Employee.java
@@ -0,0 +1,44 @@
+package com.pro.vo.child;
+
+import com.pro.vo.User;
+
+public class Employee extends User{
+	String dept;
+	double salary;
+	double bonus;
+	public Employee() {
+		super();
+	}
+	public Employee(String id, String name, String phoneNumber, String dept, double salary, double bonus) {
+		super(id, name, phoneNumber);
+		this.dept = dept;
+		this.salary = salary;
+		this.bonus = bonus;
+	}
+	
+	public String getDept() {
+		return dept;
+	}
+	
+	public void changeDept(String dept) {
+		this.dept = dept;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void changeSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public double getBonus() {
+		return bonus;
+	}
+	
+	public void changeBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	
+	
+}
---
+++ src/com/pro/vo/child/Guest.java
@@ -0,0 +1,5 @@
+package com.pro.vo.child;
+
+public class Guest {
+
+}
